be2c2955f9b99b85584e01ba252f4e3e1040c6f6
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-08-19 22:55:29 +0200

    tarjetas de la portada

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,49 @@
 import { NextPage } from 'next'
-import Link from 'next/link'
 import SliderComponent from '../components/SliderComponent'
 import HomeComponent from '../components/HomeComponent'
+import { API_URL } from '../utils'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
-  return (
+  
+
+  const [valorados, setValorados] = useState(null);
+  const [valoraciones, setValoraciones] = useState(null);
+  const [actualizados, setActualizados] = useState(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+
+  useEffect(() => {
+    const url_cursos = `${API_URL}cursos/search/selectMorePoints`;
+  // response.data._embedded.cursos
+  const url_valoraciones = `${API_URL}valoraciones/search/selectLastOpinions`;
+  //response.data._embedded.valoraciones
+  const url_actualizaciones = `${API_URL}cursos/search/selectLastUpdates`;
+  //response.data._embedded.cursos;
+    const fetchData = async () => {
+      const result_cursos = await axios(url_cursos)
+      setValorados(result_cursos.data._embedded.cursos)
+      const result_valoraciones = await axios(url_valoraciones)
+      setValoraciones(result_valoraciones.data._embedded.valoraciones)
+      const result_actualizaciones = await axios(url_actualizaciones)
+      setActualizados(result_actualizaciones.data._embedded.cursos)
+
+      setLoading(false);
+    }
+    fetchData()
+  }, [])
+  
+    //const { cursos_mas_valorados, valoraciones_cursos, cursos_actualizados } = data; 
+    const data: any = {cursos_mas_valorados: valorados, valoraciones_cursos:valoraciones, cursos_actualizados:actualizados};
+
+  return loading ? (
+    <div>...Data Loading.....</div>
+  ) : (
     <>
       <SliderComponent/>
 
-      <HomeComponent></HomeComponent>
+      <HomeComponent data={data}></HomeComponent>
       </>
   )
 }
