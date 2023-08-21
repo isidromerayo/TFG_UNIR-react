baf0cbd7990136e8b2a9ab26273a7c069e88f869
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-08-21 20:32:59 +0200

    revisión carga de JS de bootstrap en Nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import LayoutComponent from '../components/LayoutComponent'
 import '../src/app/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  
   return <>
   <LayoutComponent>
         <Component {...pageProps} />
   </LayoutComponent>
+  <Script src='/assets/vendor/bootstrap/js/bootstrap.bundle.min.js' />
+  <Script src='/assets/js/main.js'/>
   </>
 }
 
