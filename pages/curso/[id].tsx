f212affc67cfbfc2f978b61ca1e024418a9c78c4
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-08-23 19:06:03 +0200

    mis datos

--- pages/curso/[id].tsx
+++ pages/curso/[id].tsx
@@ -1,7 +1,5 @@
 import { NextPage } from 'next'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
 import { API_URL } from '../../utils'
 import Swal from 'sweetalert2';
 
